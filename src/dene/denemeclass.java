b5601000034ed06046faa175e8ffa243e6acf2d9
yoral8 <ysrorl@hotmail.com>
2023-08-08 19:47:15 +0300

    branch ekledim

--- src/dene/denemeclass.java
+++ src/dene/denemeclass.java
@@ -3,5 +3,6 @@ package dene;
 public class denemeclass {
     public static void main(String[] args) {
         System.out.println("dene master");
+        System.out.println("branch ekle");
     }
 }
