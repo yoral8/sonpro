634dabdcad1c483539a6adb6b2cd444b7118ab74
yoral8 <ysrorl@hotmail.com>
2023-08-08 19:50:26 +0300

    branch ekledim2

--- src/dene/denemeclass.java
+++ src/dene/denemeclass.java
@@ -4,5 +4,6 @@ public class denemeclass {
     public static void main(String[] args) {
         System.out.println("dene master");
         System.out.println("branch ekle");
+        System.out.println("neden olmadı");
     }
 }
